bb8fd7fdd2c09e66208d8bc7e24b59c92778bb20
itsZN <nscratch@rocketmail.com>
2012-09-01 20:20:18 -0400

    Got the carpenter working, delay is to long however

--- ForestryMaceratorCore.java
+++ ForestryMaceratorCore.java
@@ -15,7 +15,7 @@ import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import forestry.api.recipes.ICarpenterManager;
+import forestry.api.recipes.RecipeManagers;
 
 @Mod(modid = "ForestryMacerator", name = "Forestry Macerator Addon", version = "1.0.0")
 public class ForestryMaceratorCore {
@@ -34,10 +34,15 @@ public class ForestryMaceratorCore {
 	
 	@Init
 	public void initialize(FMLInitializationEvent evt) {
+		RecipeManagers.carpenterManager.addRecipe(30,new LiquidStack(Block.waterStill.blockID, 2000), null ,new ItemStack(Item.ingotIron,2),new Object[]{"DDD","DDD","DDD", Character.valueOf('D'), Block.dirt});
 	}
+	
+	public String getPriorities() {
+	    return "after:mod_IC2;after:mod_BuildCraftCore;after:mod_BuildCraftEnergy;after:mod_BuildCraftFactory;after:mod_BuildCraftSilicon;after:mod_BuildCraftTransport;after:mod_RedPowerWorld;after:mod_Forestry";
+	  }
 
 	public void ModsLoaded() {
-		ICarpenterManager.addRecipe(600,new LiquidStack(Block.waterStill.blockID, 2000), null ,new ItemStack(Item.ingotIron,2),1,1,);
+		
 	}
 
 }
